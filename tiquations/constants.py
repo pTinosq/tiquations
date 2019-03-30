0d15bfc0e35afd908a0d5ae3062552a557426ca5
Eraz <48658740+SirEraz@users.noreply.github.com>
2019-03-30 15:07:49 -0500

    Update constants.py
    Added following constants: planet distances, I naught, speed of light, avogadros num, and earth's mass

--- tiquations/constants.py
+++ tiquations/constants.py
@@ -13,22 +13,49 @@ pi = math.pi
 eulersnum= 2.7182818284590452353602874713527
 eulersmasch = 0.577215664901532860606512
 golden_ratio = 1.6180339887498948420
+sun_to_earth = 149597870 
+sun_to_mars = 227940000 
+sun_to_mercury = 57900000 
+sun_to_venus = 108200000 
+sun_to_jupiter = 778300000 
+sun_to_neptune = 4497100000 
+sun_to_saturn = 1427000000 
+sun_to_uranus = 2871000000 
+sun_to_pluto = 5913000000 
+I_naught = 0.000000000001
+speed_of_light = 299792458
+avogadros_num = 602214086000000000000000
+earth_mass = 5980000000000000000000000
 
 def variables():
     print('''
-earth_g = 9.807
-moon_g = 1.62
-mars_g = 3.711
-jupiter_g = 24.79
-neptune_g = 11.15
-mercury_g = 3.7
-saturn_g = 10.44
-uranus_g = 8.87
-venus_g = 8.87
-pluto_g = 0.62
+earth_g = 9.807 m/s^2
+moon_g = 1.62 m/s^2
+mars_g = 3.711 m/s^2
+jupiter_g = 24.79 m/s^2
+neptune_g = 11.15 m/s^2
+mercury_g = 3.7 m/s^2
+saturn_g = 10.44 m/s^2
+uranus_g = 8.87 m/s^2
+venus_g = 8.87 m/s^2
+pluto_g = 0.62 m/s^2
 pi = 3.141592653589793
 eulersnum = 2.7182818284590452353602874713527
 eulersmasch = 0.577215664901532860606512
 golden_ratio = 1.6180339887498948420
+sun_to_earth = 149,597,870 km
+sun_to_mars = 227,940,000 km
+sun_to_mercury = 57,900,000 km
+sun_to_venus = 108,200,000 km
+sun_to_jupiter = 778,300,000 km
+sun_to_neptune = 4,497,100,000 km
+sun_to_saturn = 1,427,000,000 km
+sun_to_uranus = 2,871,000,000 km
+sun_to_pluto = 5,913,000,000 km
+I_naught = 1.0 * 10^-12 W/m^2
+speed_of_light = 299792458 m/s
+avogadros_num = 6.02214086 * 10^23 mol^-1
+earth_mass =  5.98 * 10^24 kg
+
 
 ''')
